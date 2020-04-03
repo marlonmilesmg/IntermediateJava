4a4ea218c82ae0c94e307051178e317aed818a5c
Marlon Magonjo <Seven@1Seven@2>
2020-04-03 10:19:52 +0200

    Let's Go

---
+++ javapractice/src/main/java/www/technoextreme/co/zw/javapractice/Car.java
@@ -0,0 +1,13 @@
+package www.technoextreme.co.zw.javapractice;
+
+public class Car {
+
+    public String colour = "Black";
+
+    public Car(){
+
+    }
+
+
+
+}
---
+++ javapractice/src/main/java/www/technoextreme/co/zw/javapractice/CarSimulator.java
@@ -0,0 +1,12 @@
+package www.technoextreme.co.zw.javapractice;
+
+public class CarSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Car Simulator...");
+
+        Car myToyota = new Car();
+
+
+    }
+}
